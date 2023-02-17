77d5e0454f6be5b653b6076e403cd0bb98a5b2b6
Tyler Turner <tylerturner1504@gmail.com>
2023-02-17 15:27:02 -0500

    Initial commit

---
+++ src/main/java/com/tcj/spui/GetSeedGenreList.java
@@ -0,0 +1,26 @@
+package com.tcj.spui;
+
+import se.michaelthelin.spotify.requests.data.browse.miscellaneous.GetAvailableGenreSeedsRequest;
+
+import static org.fusesource.jansi.Ansi.ansi;
+
+public class GetSeedGenreList {
+    private static final GetAvailableGenreSeedsRequest getAvailableGenreSeedsRequest = Main.spotifyApi.getAvailableGenreSeeds()
+            .build();
+
+    public static void getAvailableGenreSeeds_Sync() {
+        try {
+            final String[] strings = getAvailableGenreSeedsRequest.execute();
+
+            for (String string : strings) {
+                System.out.println(string);
+            }
+        } catch (Exception e) {
+            System.out.println(ansi().render("@|red Error: " + e.getMessage() + "|@"));
+        }
+    }
+
+    public static void execute(){
+        getAvailableGenreSeeds_Sync();
+    }
+}
