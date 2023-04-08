e5ebfba2ddb9d81ceed3f4ae271709a08024b60f
tyler <tturner2021@my.fit.edu>
2023-04-08 03:11:59 -0400

    Added the ability to get the client secret and added test for it

--- src/main/java/com/tcj/spui/SpUIDatabase.java
+++ src/main/java/com/tcj/spui/SpUIDatabase.java
@@ -11,6 +11,9 @@ import java.net.http.HttpConnectTimeoutException;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class SpUIDatabase {
 
@@ -21,41 +24,86 @@ public class SpUIDatabase {
   private String authHeader;
 
 
-  public SpUIDatabase() throws URISyntaxException {
+  private HttpResponse<String> response;
+
+
+  public SpUIDatabase() throws URISyntaxException, IOException, InterruptedException {
 
     this.client = HttpClient.newHttpClient();
-
+    this.response = initalConnect();
 
   }
 
-  public HttpResponse<String> databaseResponse()
-      throws IOException, InterruptedException {
+  public HttpResponse<String> initalConnect() throws IOException, InterruptedException {
 
-    long startTime = System.nanoTime();
-    long currentTime = startTime;
-    while (currentTime < startTime + 10000) {
-      HttpResponse<String> response = this.client.send(HttpRequest.newBuilder().GET().uri(URI.create(INITIAL_ENDPOINT))
-          .headers("CF-Access-Client-Id", CFServiceToken.id, "CF-Access-Client-Secret",
-              CFServiceToken.secret).build(), HttpResponse.BodyHandlers.ofString());
+    HttpResponse<String> response;
+    long startTime = System.currentTimeMillis();
+    long currentTime;
+    do {
+      response = this.client.send(
+          HttpRequest.newBuilder().GET().uri(URI.create(INITIAL_ENDPOINT))
+              .headers("CF-Access-Client-Id", Secrets.id, "CF-Access-Client-Secret",
+                  Secrets.secret).build(), HttpResponse.BodyHandlers.ofString());
       if (response.statusCode() == 200) {
         return response;
       }
-      currentTime = System.nanoTime();
+      currentTime = System.currentTimeMillis();
     }
-    throw new HttpConnectTimeoutException("Could not connect to database");
+    while (currentTime < (startTime + 5000));
+
+    switch (response.statusCode()) {
+      case 401 -> {
+        throw new HttpConnectTimeoutException(
+            String.format("\nCode %d\nPermission Denied", response.statusCode()));
+      }
+      case 404 -> {
+        throw new HttpConnectTimeoutException(
+            String.format("\nCode %d\nCannot find server", response.statusCode()));
+      }
+      default -> {
+        throw new HttpConnectTimeoutException(
+            String.format("\nCode %d\nCould not connect to server", response.statusCode()));
+      }
+    }
   }
 
+  public static HashMap<String, String> stripResponse(HttpResponse<String> response) throws Exception {
+
+    ArrayList<String> responseAsList = new ArrayList<>(
+        List.of(response.body().replaceAll("[\\[\\]{\"}]", "").split(":")));
+    if (responseAsList.size() % 2 != 0) {
+      throw new Exception("Response not an even number");
+    }
+    HashMap<String, String> outputMap = new HashMap<>();
+    for (int i = 0; i < responseAsList.size(); i += 2) {
+      outputMap.put(responseAsList.get(i), responseAsList.get(i+1));
+    }
+    return outputMap;
+  }
+
+  public String getClientSecret()
+      throws Exception {
+
+    return stripResponse(this.client.send(HttpRequest.newBuilder().GET().uri(new URI(INITIAL_ENDPOINT + "/app")).headers("CF-Access-Client-Id", Secrets.id, "CF-Access-Client-Secret",
+        Secrets.secret).build(), HttpResponse.BodyHandlers.ofString())).get("client_secret");
+
+  }
+
+  public HttpResponse<String> getResponse() {
+    return response;
+  }
+
+  public void setResponse(HttpResponse<String> response) {
+    this.response = response;
+  }
 
   public static void main(String[] args)
-      throws IOException, InterruptedException, URISyntaxException {
+      throws Exception {
 
     SpUIDatabase db = new SpUIDatabase();
+    System.out.println(db.response.body());
 
-    HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(INITIAL_ENDPOINT))
-        .headers("CF-Access-Client-Id", CFServiceToken.id, "CF-Access-Client-Secret",
-            CFServiceToken.secret).build();
-    HttpResponse<String> response = db.client.send(request, HttpResponse.BodyHandlers.ofString());
-    System.out.println(response.statusCode());
+    System.out.println(db.getClientSecret());
   }
 
 }
--- src/test/java/SpUIDatabaseTest.java
+++ src/test/java/SpUIDatabaseTest.java
@@ -1,13 +1,17 @@
 import static com.tcj.spui.SpUIDatabase.INITIAL_ENDPOINT;
 
-import com.tcj.spui.CFServiceToken;
+import com.tcj.spui.Secrets;
 import com.tcj.spui.SpUIDatabase;
 import java.io.IOException;
+import java.lang.reflect.MalformedParameterizedTypeException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
 import org.junit.*;
 
+import static com.tcj.spui.SpUIDatabase.stripResponse;
 import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.Assertions;
@@ -15,22 +19,25 @@ import org.junit.jupiter.api.Test;
 
 public class SpUIDatabaseTest {
 
-//  @Test
-//  public void testNoAuthHeader() throws URISyntaxException, IOException, InterruptedException {
-//
-//    SpUIDatabase db = new SpUIDatabase();
-//    HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(INITIAL_ENDPOINT)).build();
-//    Assertions.assertEquals(401, db.databaseResponse().statusCode());
-//
-//  }
-
   @Test
   public void testSuccessfulGetOnInitEndpoint()
       throws URISyntaxException, IOException, InterruptedException {
 
     SpUIDatabase db = new SpUIDatabase();
-    Assertions.assertEquals(200, db.databaseResponse().statusCode());
+    Assertions.assertEquals(200, db.getResponse().statusCode());
 
+  }
+
+  @Test
+  public void testGetClientSecret() throws Exception {
+    SpUIDatabase db = new SpUIDatabase();
+    Assertions.assertEquals(Secrets.appSecret, db.getClientSecret());
+  }
+
+  @Test
+  public void testStripResponse() throws Exception {
+    SpUIDatabase db = new SpUIDatabase();
+    Assertions.assertInstanceOf(HashMap.class, stripResponse(db.getResponse()));
   }
 
 }
