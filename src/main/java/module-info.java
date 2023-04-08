ec1bd051a128b00b258cd367caab482aa0124f3a
tyler <tturner2021@my.fit.edu>
2023-04-08 16:46:14 -0400

    comment out hansolo pt 2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,7 +9,7 @@ module com.tcj.spui {
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
+//    requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
     requires se.michaelthelin.spotify;
     requires org.fusesource.jansi;
