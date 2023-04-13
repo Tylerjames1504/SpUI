9bce9118456552f3ae98ed10a70f2c7c77d91839
tyler <tturner2021@my.fit.edu>
2023-04-12 20:51:44 -0400

    merge successful

--- src/main/java/com/tcj/spui/HelloApplication.java
+++ src/main/java/com/tcj/spui/HelloApplication.java
@@ -23,6 +23,5 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
-        Main.run();
     }
 }
--- src/main/java/com/tcj/spui/SpUIDatabase.java
+++ src/main/java/com/tcj/spui/SpUIDatabase.java
@@ -191,10 +191,6 @@ public class SpUIDatabase {
 
 
 
-
-
-
-
   }
 
 }
