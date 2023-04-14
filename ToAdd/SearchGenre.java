8a3b65586866a4eb71e6090bfe7ab538c81ce06b
Charles Derbes <charles@derbes.net>
2023-04-14 02:03:58 -0400

    added sam's files for functionality reference to a folder under the project

---
+++ ToAdd/SearchGenre.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+import static org.fusesource.jansi.Ansi.ansi;
+
+public class SearchGenre {
+    private static String alphabet = "";
+    public static void execute(){
+        Scanner input = new Scanner(System.in);
+        System.out.println(ansi().render("@|green Please enter the starting alphabet of the genre to search|@"));
+        alphabet = input.nextLine();
+        while(alphabet.length() > 1){
+            System.out.println(ansi().render("@|green Please enter the starting alphabet only!\nPlease enter the alphabet again:|@"));
+            alphabet = input.nextLine();
+        }
+        try{
+            final String[] strings = MainHELLO.spotifyApi.getAvailableGenreSeeds().build().execute();
+            char[] genreChar;
+            char[] alphabetChar = alphabet.toCharArray();
+            System.out.println();
+            for (String genre : strings) {
+                genreChar = genre.toCharArray();
+                if (genreChar[0] == alphabetChar[0]) {
+                    System.out.println(genre);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(ansi().render("@|red Error: " + e.getMessage() + "|@"));
+        }
+    }
+}
---
+++ ToAdd/module-info.java
@@ -0,0 +1,23 @@
+module com.tcj.spui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires javafx.swing;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires se.michaelthelin.spotify;
+    requires org.fusesource.jansi;
+    requires nv.i18n;
+    requires org.apache.httpcomponents.core5.httpcore5;
+    requires com.google.gson;
+
+    opens com.tcj.spui to javafx.fxml;
+    exports com.tcj.spui;
+
+}
