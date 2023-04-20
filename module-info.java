b1b55036dd260392b88ab639aad06907302aecf9
tyler <tturner2021@my.fit.edu>
2023-04-20 18:59:15 -0400

    remove hansolo

--- module-info.java
+++ module-info.java
@@ -9,7 +9,6 @@ module com.example.demo {
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
     requires org.fusesource.jansi;
     requires se.michaelthelin.spotify;
