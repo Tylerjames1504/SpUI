88225c93c4fbada860fc7c261d1ab7f727f10e2e
tyler <tturner2021@my.fit.edu>
2023-04-20 22:16:28 -0400

    adding mac address

---
+++ src/main/java/com/tcj/spui/Encryption.java
@@ -0,0 +1,102 @@
+package com.tcj.spui;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Arrays;
+import java.util.Base64;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Encryption {
+
+  public static SecretKey getKeyFromPassword(String password, String salt)
+      throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+    SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+    KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
+    return new SecretKeySpec(factory.generateSecret(spec)
+        .getEncoded(), "AES");
+  }
+
+  public static IvParameterSpec generateIv() {
+    byte[] iv = new byte[16];
+    new SecureRandom().nextBytes(iv);
+    return new IvParameterSpec(iv);
+  }
+
+  public static String encrypt(String algorithm, String input, SecretKey key,
+      IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException,
+      InvalidAlgorithmParameterException, InvalidKeyException,
+      BadPaddingException, IllegalBlockSizeException {
+
+    Cipher cipher = Cipher.getInstance(algorithm);
+    cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+    byte[] cipherText = cipher.doFinal(input.getBytes());
+    return Base64.getEncoder()
+        .encodeToString(cipherText);
+  }
+
+  public static String decrypt(String algorithm, String cipherText, SecretKey key,
+      IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException,
+      InvalidAlgorithmParameterException, InvalidKeyException,
+      BadPaddingException, IllegalBlockSizeException {
+
+    Cipher cipher = Cipher.getInstance(algorithm);
+    cipher.init(Cipher.DECRYPT_MODE, key, iv);
+    byte[] plainText = cipher.doFinal(Base64.getDecoder()
+        .decode(cipherText));
+    return new String(plainText);
+  }
+
+  public static void main(String[] args)
+      throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, SocketException, UnknownHostException {
+
+    System.out.println(GetNetworkAddress.GetAddress("mac"));
+
+    String input = "baeldung";
+    SecretKey key = getKeyFromPassword("00:E0:4C:8A:1A:61", "00:E0:4C:8A:1A:61");
+    IvParameterSpec ivParameterSpec = Encryption.generateIv();
+    String algorithm = "AES/CBC/PKCS5Padding";
+    String cipherText = Encryption.encrypt(algorithm, input, key, ivParameterSpec);
+    String plainText = Encryption.decrypt(algorithm, cipherText, key, ivParameterSpec);
+    System.out.println(cipherText);
+    System.out.println(plainText);
+    System.out.println(input.equals(plainText));
+
+  }
+
+
+  public Encryption() {
+  }
+
+  public String encrypt(String input) {
+    return input;
+  }
+
+  public String decrypt(String input) {
+    return input;
+  }
+
+}
